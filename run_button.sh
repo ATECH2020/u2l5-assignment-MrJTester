e4901f0cec6d8c84d4a939e54a66d01630f15d99
jacksj12 <30806811+jacksj12@users.noreply.github.com>
2020-09-22 07:09:38 -0700

    Initial commit

---
+++ run_button.sh
@@ -0,0 +1,5 @@
+javac src/main/java/Construction.java
+javac src/main/java/ConstructionTester.java
+javac src/main/java/HowFarAway.java
+javac src/main/java/GeoLocation.java
+java -cp src/main/java/ ConstructionTester
