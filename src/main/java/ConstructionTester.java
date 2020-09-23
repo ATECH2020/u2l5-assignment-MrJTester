88bf4a1bd74b19a8b53972dac6300a9f36be2a85
repl.it user <replituser@example.com>
2020-09-23 17:20:53 +0000

    period 2

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -3,31 +3,15 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        int numBoards, numWindows;
         double taxRate;
+				int numBoards;
+				int numWindows;
 
-        Scanner input = new Scanner(System.in);
+				Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter the sales tax rate: ");
-        taxRate = input.nextDouble();
-
-        System.out.println("How many boards do you need? ");
-        numBoards = input.nextInt();
-
-        System.out.println("How many windows do you need? ");
-        numWindows = input.nextInt();
-
-
-        double total = 0;
-        Construction calc = new Construction(8, 11, taxRate);
-
-        total += calc.lumberCost(numBoards);
-        total += calc.windowCost(numWindows);
-
-        System.out.print("Total: ");
-        System.out.println(total);
-
-        System.out.print("Grand Total: ");
-        System.out.print(calc.grandTotal(total));
+				System.out.print("Enter the sales tax rate: ");
+				
+				Construction myConstruction = new Construction();
+				
     }
 }
